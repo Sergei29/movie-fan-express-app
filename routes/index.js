f815dd0daf96e06f6b5396b6caba11e4d1653c74
Serge Basangovs <serge.basangovs@bbc.com>
2023-07-10 16:45:28 +0100

    feat: movie by id endpoint api

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const { fetchData } = require("../utils/fetch");
+const { formatResults } = require("../utils/adaptors");
 const { THE_MOVIE_DB_API_KEY, THE_MOVIE_DB_BASE_URL } = require("../constants");
 
 const router = express.Router();
 
 const nowPlayingUrl = `${THE_MOVIE_DB_BASE_URL}/movie/now_playing?api_key=${THE_MOVIE_DB_API_KEY}`;
-const imageBaseUrl =
-  "https://image.tmdb.org/t/p/w300/1E5baAaEse26fej7uHcjOgEE2t2.jpg";
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
   const { data, error } = await fetchData(nowPlayingUrl, { method: "GET" });
-  console.log({ data, error });
+
   if (error) {
     res.render("error", { message: error });
     return;
   }
 
-  res.json(data);
+  res.render("index", { movies: formatResults(data.results) });
+});
+
+router.get("/movie/:movieId", async (req, res, next) => {
+  const movieId = req.params.movieId;
+  const movieUrl = `${THE_MOVIE_DB_BASE_URL}/movie/${movieId}?api_key=${THE_MOVIE_DB_API_KEY}`;
+  const { data, error } = await fetchData(movieUrl, { method: "GET" });
+  if (error) {
+    res.render("error", { message: error, error });
+    return;
+  }
+
+  console.log("data :>> ", data);
+
+  res.render("single-movie");
 });
 
 module.exports = router;
